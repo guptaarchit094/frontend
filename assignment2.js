60bc84eca26ad9bb4db5dfe48d75547cfa85f915
Archit Gupta <guptaarchit094@gmail.com>
2023-04-16 17:56:03 +0530

    Assignment2

---
+++ assignment2.js
@@ -0,0 +1,30 @@
+function increament_1(){
+var a=Number(document.getElementById("a").innerText);
+a=a+1;
+document.getElementById("a").innerText=a;
+}
+
+function decreament_1(){
+    var a=Number(document.getElementById("a").innerText);
+    a=a-1;
+    document.getElementById("a").innerText=a;
+    }
+
+    function increament_5(){
+        var a=Number(document.getElementById("a").innerText);
+        a=a+5;
+        document.getElementById("a").innerText=a;
+        }
+
+        function decreament_10(){
+            var a=Number(document.getElementById("a").innerText);
+            a=a-10;
+            document.getElementById("a").innerText=a;
+            }
+
+function random(){
+    var a=Number(document.getElementById("a").innerText);
+    var b=Number(document.getElementById("b").value);
+a=a+b;
+document.getElementById("a").innerText=a;
+}
